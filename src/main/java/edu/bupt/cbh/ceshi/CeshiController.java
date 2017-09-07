f4eb0f12263f72a45eca7704210aeb475c4c28b5
changbohong <635955010@qq.com>
2017-09-07 10:59:00 +0800

    修改测试数据

--- src/main/java/edu/bupt/cbh/ceshi/CeshiController.java
+++ src/main/java/edu/bupt/cbh/ceshi/CeshiController.java
@@ -1,7 +1,10 @@
 package edu.bupt.cbh.ceshi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,13 +17,17 @@ import java.util.Map;
 public class CeshiController {
 
     @RequestMapping("/login")
-    public Map<String, Object> login(String username, String password) {
+    @ResponseBody
+    public Map<String, Object> login(@RequestBody Map<String,Object> input) {
         Map<String, Object> map = new HashMap<>();
-        if (username == "cbh" && password == "cbh") {
+        String username = (String) input.get("username");
+        String password = (String) input.get("password");
+        if (username.equals("cbh") && password.equals("cbh")) {
             map.put("code", "0");
         } else {
             map.put("code", "1");
         }
+        System.out.print(map);
         return map;
     }
 }
